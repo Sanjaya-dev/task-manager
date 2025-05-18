a196f35daf22ce98e8e8a41b5d14a84267f89ebd
Sanjaya <fernanda.forproject@gmail.com>
2025-05-18 21:26:22 +0700

    feat : create task controller and models migration

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Inertia\Inertia;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        $task = Auth::user()->tasks()->orderBy('created_at', 'desc')->get();
+        return inertia::render('Tasks/Index',[
+            'tasks' => $task
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+        ]);
+
+        Auth::user()->tasks()->create([
+            $request->only('title','description')
+        ]);
+
+        return redirect()->route('tasks.index');
+    }
+
+    public function update(Request $request, Task $task)
+    {
+        $this->authorize('update', $task);
+
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'completed' => 'boolean',
+        ]);
+
+        $task->update($request->only('title', 'description', 'completed'));
+
+        return redirect()->route('tasks.index');
+        
+    }
+
+    public function destroy(Task $task)
+    {
+        $this->authorize('delete', $task);
+        $task->delete();
+        
+        return redirect()->route('tasks.index');
+    }
+}
